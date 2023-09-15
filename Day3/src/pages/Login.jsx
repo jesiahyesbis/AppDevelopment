6e791ee6562b1a3360b64775069585433d6565d4
jesiahyesbis <110587876+jesiahyesbis@users.noreply.github.com>
2023-09-15 14:20:22 +0530

    Add files via upload

---
+++ Day3/src/pages/Login.jsx
@@ -0,0 +1,67 @@
+import ResponsiveAppBar from "../components/Navbar";
+import '../assets/css/login.css';
+import {useState} from 'react'
+import toast, {Toaster} from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import HolidayVillageIcon from '@mui/icons-material/HolidayVillage';
+const Login = () => {
+    const nav = useNavigate()
+    const [login,setLogin]=useState({
+        email:'',
+        password:''
+    })
+    const handleChange=(e)=>{
+        e.preventDefault();
+        setLogin({...login,[e.target.id]:e.target.value})
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log('Email : ${login.email} Password: ${login.password}')
+        toast.success('Successfully submitted!')
+    }
+    const handleRoute=()=>{
+       nav('/register')
+    }
+    
+    
+    return (
+        <>
+        <ResponsiveAppBar/>
+    <div className="logbg">
+        
+        <div class="container">
+    <div class="card">
+      <div class="front"></div>
+      <div class="back">
+        <h1>WELCOME TO</h1>
+        <h1 className="head1">MERCADO</h1>
+        <p className="para1">BOOK YOUR APPLIANCE</p>
+        <p className="para2">IN THE EASIEST WAY</p>
+      </div>
+    </div>
+  </div>
+
+      
+    <form onSubmit={handleSubmit}>
+    <div className="auth-form d-flex-c">
+        <input type="email" name="email" id="email" className='input-field' placeholder='Email' onChange={handleChange} autoFocus required/>
+        <input type="password" name="password" id="password" className='input-field' placeholder='Password' onChange={handleChange}  required/>
+        <button type="submit" className='auth-btn'>Login</button>
+    </div>
+    <h5 className="regbut">Route to <span className='text-link' onClick={handleRoute}>Register ?</span></h5>
+    </form>
+    </div>
+    <Toaster
+         position="top-center"
+         reverseOrder={false}
+    />  
+       
+      
+   
+    </>
+
+
+  )
+}
+
+export default Login
